42a2a577a6f673c0030238139bbcfb75def0353e
Knirag <105249761+Knirag@users.noreply.github.com>
2023-07-28 07:53:54 +0200

    ...

--- lib.js
+++ lib.js
@@ -1,4 +1,3 @@
-// Object that holds information about books
 const books = {
   'treasure-island': {
     title: 'Treasure Island',
@@ -24,16 +23,47 @@ const books = {
     title: "Gulliver's Travels",
     description: 'A satirical novel by Jonathan Swift, featuring the adventures of Lemuel Gulliver.',
     cover: 'gullivers-travels.jpg'
+  },
+  'count-of-monte-cristo': {
+    title: 'The Count of Monte Cristo',
+    description: 'A historical adventure novel by Alexandre Dumas, set in early 19th-century France and Italy.',
+    cover: 'monte-christo.jpg'
+  },
+  'little-women': {
+    title: 'Little Women',
+    description: 'A novel by Louisa May Alcott, following the lives of the four March sisters.',
+    cover: 'little-women.jpg'
   }
 };
-/*
+
 // Function to display book information
 function showBookInfo(bookId) {
   const book = books[bookId];
   const bookCover = document.getElementById('bookCover');
   const bookTitle = document.getElementById('bookTitle');
   const bookDescription = document.getElementById('bookDescription');
-*/
+
+  bookCover.src = book.cover;
+  bookTitle.textContent = book.title;
+  bookDescription.textContent = book.description;
+}
+
+// Function to change the displayed book based on the selected item
+function changeBook(bookId) {
+  showBookInfo(bookId);
+  displayReviews(bookId); // Display reviews for the selected book
+}
+
+// click event listener to each book item in the sidebar
+const bookItems = document.querySelectorAll('.sidebar ul li');
+bookItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    const bookId = item.getAttribute('data-book'); // Get the book ID from the data-book attribute
+    changeBook(bookId);
+    bookItems.forEach((item) => item.classList.remove('active'));
+    item.classList.add('active');
+  });
+});
 
 // Array to store reviews
 const reviews = [];
@@ -50,13 +80,12 @@ function addReview(name, review) {
   // Clear the input fields after submission
   document.getElementById('reviewerName').value = '';
   document.getElementById('reviewContent').value = '';
-
   // Display the reviews
   displayReviews();
 }
 
-// Function to display reviews
-function displayReviews() {
+// Function to display reviews for the selected book
+function displayReviews(bookId) {
   const submittedReviewsElement = document.getElementById('submittedReviews');
   submittedReviewsElement.innerHTML = ''; // Clear previous reviews
 
@@ -85,16 +114,26 @@ function handleFormSubmit(event) {
 
   // Add the review
   addReview(name, review);
-
-  // Display the updated reviews
-  displayReviews();
-
-  // Clear the input fields after submission
-  document.getElementById('reviewerName').value = '';
-  document.getElementById('reviewContent').value = '';
 }
 
 // Add an event listener to the form submission
 const reviewForm = document.getElementById('reviewForm');
 reviewForm.addEventListener('submit', handleFormSubmit);
 
+// Function to toggle the review form visibility
+function toggleReviewForm() {
+  const reviewForm = document.getElementById('reviewForm');
+  const addReviewButton = document.getElementById('addReviewButton');
+
+  if (reviewForm.style.display === 'block') {
+    reviewForm.style.display = 'none';
+    addReviewButton.innerText = 'Add Review';
+  } else {
+    reviewForm.style.display = 'block';
+    addReviewButton.innerText = 'Cancel';
+  }
+}
+
+// Add an event listener to the "Add Review" button click
+const addReviewButton = document.getElementById('addReviewButton');
+addReviewButton.addEventListener('click', toggleReviewForm);
